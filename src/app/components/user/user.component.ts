799a51fe44af52146df7e0260443b0bdd58c206d
harika <harika26.gujjula@gmail.com>
2024-11-14 16:30:50 -0500

    Usign signals rather than change detection with zone.js.

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal, computed } from '@angular/core';
 import { DUMMY_USERS } from '../../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -11,14 +11,16 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
+  // Defining selectedUser as a signal(supported with Angular 16+).
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
 
-  get imagePath () {
-    return 'users/' + this.selectedUser.avatar
-  }
+  // Use computed() to get a value based on signal, so that the value is
+  //  computed only if the selectedUser signal changes.
+  imagePath = computed(() => 'users/' + this.selectedUser().avatar)
 
   onSelectUser() {
     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser = DUMMY_USERS[randomIndex];
+    // Signal should be set by using set().
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
   }
 }
