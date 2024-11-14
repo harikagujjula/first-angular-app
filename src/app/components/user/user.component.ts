0133e0080bb9633bf4d71e0722d4331781f53315
harika <harika26.gujjula@gmail.com>
2024-11-14 17:06:08 -0500

    Rendering all users using user component each time manually.

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,7 +1,4 @@
-import { Component, signal, computed } from '@angular/core';
-import { DUMMY_USERS } from '../../dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -11,16 +8,13 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
-  // Defining selectedUser as a signal(supported with Angular 16+).
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
+  @Input() avatar!: string;
+  @Input() name!: string;
 
-  // Use computed() to get a value based on signal, so that the value is
-  //  computed only if the selectedUser signal changes.
-  imagePath = computed(() => 'users/' + this.selectedUser().avatar)
+  get imagePath() {
+    return 'users/' + this.avatar;
+  }
 
   onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    // Signal should be set by using set().
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
   }
 }
