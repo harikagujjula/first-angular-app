e91f2e5c86f2316b6b71f6f1f789ba82f9f99274
harika <harika26.gujjula@gmail.com>
2024-11-14 16:14:20 -0500

    Adding button click to update user on each click.

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { DUMMY_USERS } from '../../dummy-users';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -15,5 +15,10 @@ export class UserComponent {
 
   get imagePath () {
     return 'users/' + this.selectedUser.avatar
+  }
+
+  onSelectUser() {
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser = DUMMY_USERS[randomIndex];
   }
 }
